e4d448734180c3904bd777e2afc39cd3a6f53dc3
wy <ifinetoo@qq.com>
2023-07-02 21:21:40 +0800

    core: init git admin

---
+++ cyber-todo-admin/src/main/java/com/wy/cybertodoadmin/CyberTodoAdminApplication.java
@@ -0,0 +1,13 @@
+package com.wy.cybertodoadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberTodoAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyberTodoAdminApplication.class, args);
+    }
+
+}
---
+++ cyber-todo-admin/src/test/java/com/wy/cybertodoadmin/CyberTodoAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wy.cybertodoadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberTodoAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
